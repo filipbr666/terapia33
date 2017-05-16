da5c47d5a3424ef8b1987b4d7e0aceaa34e5e9d5
Filip Barnigiel <filipbr666@gmail.com>
2017-05-16 19:23:25 +0200

    Add project files.

---
+++ terapia33/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace terapia33
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PicBoxMovTimer = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PicBoxMovTimer
+            // 
+            this.PicBoxMovTimer.Interval = 10;
+            this.PicBoxMovTimer.Tick += new System.EventHandler(this.PicBoxMovTimer_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Obrazek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.WybierzObrazek);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 468);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer PicBoxMovTimer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ terapia33/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace terapia33
+{
+    public partial class Form1 : Form
+    {
+        Image file;
+        int imageX;
+        int imageY;
+        int program = 1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (System.Reflection.PropertyInfo prop in typeof(Color).GetProperties())
+            {
+                if (prop.PropertyType.FullName == "System.Drawing.Color")
+                    comboBox1.Items.Add(prop.Name);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string color = comboBox1.Text;
+            this.BackColor = Color.FromName(color);
+        }
+
+        private void WybierzObrazek(object sender, EventArgs e)
+        {
+            OpenFileDialog image = new OpenFileDialog();
+            image.Filter = "JPG (*.jpg)|*.jpg";
+            if (image.ShowDialog() == DialogResult.OK)
+            {
+                PicBoxMovTimer.Enabled = false;
+                program = 1;
+                file = Image.FromFile(image.FileName);
+                imageX = 0;
+                imageY = this.Height / 2;
+                pictureBox1.Location = new Point(imageX - pictureBox1.Width, imageY - pictureBox1.Height / 2);
+
+                pictureBox1.Image = file;
+                PicBoxMovTimer.Enabled = true;
+            }
+        }
+
+        private void PicBoxMovTimer_Tick(object sender, EventArgs e)
+        {
+            switch (program)
+            {
+                case 1:
+                    imageX = imageX + 4;
+                    if (imageX > this.Width + pictureBox1.Width)
+                        program = 2;
+                    break;
+                case 2:
+                    imageX = imageX - 4;
+                    if (imageX < 4)
+                    {
+                        program = 3;
+                        imageX = this.Width / 2;
+                        imageY = 0;
+                        pictureBox1.Location = new Point(imageX - pictureBox1.Width / 2, imageY - pictureBox1.Height);
+                    }
+                    break;
+                case 3:
+                    imageY = imageY + 4;
+                    if (imageY > this.Height + pictureBox1.Height)
+                        program = 4;
+                    break;
+                case 4:
+                    imageY = imageY - 4;
+                    if (imageY < 4)
+                    {
+                        program = 5;
+                        imageX = 0;
+                        imageY = 0;
+                        pictureBox1.Location = new Point(imageX - pictureBox1.Width, imageY - pictureBox1.Height);
+                    }
+                    break;
+                case 5:
+                    imageX = imageX + 4;
+                    imageY = imageY + 4;
+                    if (imageX > this.Width + pictureBox1.Width && imageY > this.Height + pictureBox1.Height)
+                    {
+                        program = 6;
+                        imageX = this.Width;
+                        imageY = 0;
+                        pictureBox1.Location = new Point(imageX, imageY - pictureBox1.Height);
+                    }
+                    break;
+                case 6:
+                    imageX = imageX - 4;
+                    imageY = imageY + 4;
+                    if (imageX < 4)
+                    {
+                        program = 1;
+                        imageX = 0;
+                        imageY = this.Height / 2;
+                        pictureBox1.Location = new Point(imageX - pictureBox1.Width, imageY - pictureBox1.Height / 2);
+                    }
+                    break;
+                default:
+                    program = 1;
+                    imageX = 0;
+                    imageY = this.Height / 2;
+                    pictureBox1.Location = new Point(imageX - pictureBox1.Width, imageY - pictureBox1.Height / 2);
+                    break;
+            }
+
+            pictureBox1.Location = new Point(imageX - pictureBox1.Width, imageY - pictureBox1.Height);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Escape)
+            {
+                PicBoxMovTimer.Enabled = false;
+                program = 1;
+                Close();
+            }
+        }
+    }
+}
